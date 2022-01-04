c38e746a0082b93b6c1798c88cb3130707ea2d43
Jayakumar <jayakumargowtham2812@gmail.com>
2022-01-04 23:28:43 +0530

    Init commit

---
+++ config.js
@@ -0,0 +1,2 @@
+export const nftaddress = "0x08842fa17d596DD67050dA1e7621C7F7DF7D5902"
+export const nftmarketaddress = "0x20F1EeDFCB0c5b6d2aFbcB8e32848AE8a40E5465"
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,36 @@
 import '../styles/globals.css'
-
+import Link from 'next/link'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className='border-b p-6'>
+        <p className='text-4xl font-bold text-center'> FutureVerse </p>
+        <div className='flex mt-4'>
+          <Link href='/'>
+            <a className='mr-6 text-blue-800'>
+              Home
+            </a>
+          </Link >
+          <Link  href='/create-item'>
+            <a className='mr-6 text-blue-800'>
+              Create NFT 
+            </a>
+          </Link >
+          <Link  href='/my-assets'>
+            <a className='mr-6 text-blue-800'>
+              Owned NFT&apos;s 
+            </a>
+          </Link >
+          <Link  href='/creator-dashboard'>
+            <a className='mr-6 text-blue-800'>
+              Creator Dashboard 
+            </a>
+          </Link >
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  ) 
 }
 
 export default MyApp
